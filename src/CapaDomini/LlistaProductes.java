88a11a4ad5014530db45214c3ee526a0dee022cd
guillem <guillem@guillem-U33Jc.(none)>
2013-10-23 18:46:33 +0200

    added gitignore

---
+++ src/CapaDomini/LlistaProductes.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package CapaDomini;
+
+/**
+ *
+ * @author guillem
+ */
+public class LlistaProductes {
+    private Producte[] ArrayProductes;
+    private int contaproductes = 0;
+    
+    public LlistaProductes(){};
+    
+    public void LlegitLlistaProductes(Producte[] ArrayProductes, int contaproductes){
+        
+    }
+}
