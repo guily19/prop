6b36153553ecdee3f5bd334d452bd731504b1b92
guillem <guillemcasanova@gmail.com>
2013-11-07 18:30:22 +0100

    Afegida la clase GrauMinim.java que es el struct de Graf.caminsminims

---
+++ src/CapaDomini/GrauMinim.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package CapaDomini;
+
+
+public class GrauMinim{
+    public int id1;
+    public int id2;
+    public int grau;
+    
+}
