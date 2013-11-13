bb0dbe7f8e623e7b07d1e8d534fa78d8473eb159
Toni <tonibuc9@gmail.com>
2013-11-13 19:29:01 +0100

    CtrlCataleg(falta Prestatge), conecta CapaDomini y CapaDades

--- src/CapaDomini/CtrlCataleg.java
+++ src/CapaDomini/CtrlCataleg.java
@@ -20,6 +20,9 @@ public class CtrlCataleg {
     private final ArrayList<Producte> productes;
     private final ArrayList<LlistaProductes> llistes;
     private final ArrayList<Prestatge> prestatges;
+    private int nextProd;
+    private int nextLlis;
+    private int nextPres;
     
     /**
      * Creadora de la clase
@@ -29,8 +32,13 @@ public class CtrlCataleg {
         llistes = new ArrayList();
         prestatges = new ArrayList();
         ctrl = new ControladorDades();
+        nextProd = 0;
+        nextLlis = 0;
+        nextPres = 0;
     }
     
+    
+    //#####Consultores####
     public ArrayList<Producte> getProductes(){
         return this.productes;
     }
@@ -43,12 +51,77 @@ public class CtrlCataleg {
         return this.prestatges;
     }
     
-    public void saveProductes(){
-        
+     /**
+     * @return the nextProd
+     */
+    public int getNextProd() {
+        return nextProd;
+    }
+
+    /**
+     * @return the nextLlis
+     */
+    public int getNextLlis() {
+        return nextLlis;
+    }
+
+    /**
+     * @return the nextPres
+     */
+    public int getNextPres() {
+        return nextPres;
+    }
+
+    //#####Modificadores######
+    
+    public void setProducte(Producte p){
+        int i = p.getId();
+        if(i == nextProd) ++nextProd;
+        productes.add(i,p);
     }
     
-    public void saveLlistes(){
-        
+    public void setLlista(LlistaProductes ll){
+        int i = ll.getId();
+        if(i == nextLlis) ++nextLlis;
+        llistes.add(i,ll);
+    }
+    
+    public void setPrestatge(Prestatge p){
+        int i = p.getId();
+        if(i == nextPres) ++nextPres;
+        prestatges.add(i,p);
+    }
+    
+    //#####I/O a Fixers#####
+    public void saveProductes() throws IOException{
+        ArrayList<String[]> aux = new ArrayList();
+        for(int i = 0; i < nextProd; ++i){
+            String[] s = new String[5];
+            Producte p = productes.get(i);
+            s[0] = Integer.toString(p.getId());
+            s[1] = p.getNom();
+            s[2] = p.getTipus();
+            s[3] = p.getSubtipus();
+            s[4] = p.getConservacio();
+            aux.add(s);
+        }
+        ctrl.writeFile(aux, "./Dades/Productes.data");
+    }
+    
+    public void saveLlistes() throws IOException{
+        ArrayList<String[]> aux = new ArrayList();
+        for(int i = 0; i < nextLlis; ++i){
+            LlistaProductes ll = llistes.get(i);
+            String[] s = new String[ll.getContaproductes() + 3];
+            s[0] = Integer.toString(ll.getId());
+            s[1] = ll.getNom();
+            int n;
+            s[2] = Integer.toString(n = ll.getContaproductes());
+            ArrayList<Integer> aux2 = ll.getArrayproductes();
+            for(int j = 0; j < n; ++j) s[j + 3] =Integer.toString(aux2.get(j));
+            aux.add(s);
+        }
+        ctrl.writeFile(aux, "./Dades/Llistes.data");
     }
     
     public void savePrestatges(){
@@ -57,8 +130,8 @@ public class CtrlCataleg {
     
     public void loadProductes() throws IOException{
         ArrayList<String[]> aux = ctrl.readFile("./Dades/Productes.data");
-        int n = aux.size();
-        for(int i = 0; i < n; ++i){
+        nextProd = aux.size();
+        for(int i = 0; i < nextProd; ++i){
             String[] s = aux.get(i);
             Producte p = new Producte(Integer.parseInt(s[0]), s[1], s[2], s[3], s[4]);
             productes.add(p);
@@ -67,15 +140,23 @@ public class CtrlCataleg {
     
     public void loadLlistes() throws IOException{
         ArrayList<String[]> aux = ctrl.readFile("./Dades/Llistes.data");
-        int n = aux.size();
-        for(int i = 0; i < n; ++i){
+        nextLlis = aux.size();
+        for(int i = 0; i < nextLlis; ++i){
             String[] s = aux.get(i);
-            Producte p = new Producte(Integer.parseInt(s[0]), s[1], s[2], s[3], s[4]);
-            productes.add(p);
+            int n = s.length;
+            LlistaProductes ll = new LlistaProductes();
+            ll.setId(Integer.parseInt(s[0]));
+            ll.setNom(s[1]);
+            ll.setContaproductes( Integer.parseInt(s[2]));
+            ArrayList<Integer> aux2 = new ArrayList();
+            for(int j = 3; j < n; ++j) aux2.add(Integer.parseInt(s[j]));
+            ll.setArrayproductes(aux2);
+            llistes.add(ll);
         }
     }
     
     public void loadPrestatges(){
     
     }
-}
+
+ 
