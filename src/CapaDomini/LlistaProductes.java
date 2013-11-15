e11d1e6122febcedca0cad11649dc7df0ecdc70f
guillem <guillemcasanova@gmail.com>
2013-11-15 15:15:34 +0100

    Afegida la matriu similituds a la llistaproducte

--- src/CapaDomini/LlistaProductes.java
+++ src/CapaDomini/LlistaProductes.java
@@ -18,22 +18,60 @@ public class LlistaProductes {
     private String nom;
     private ArrayList<Integer> arrayproductes = new ArrayList();
     private int contaproductes = 0;
+    private final ArrayList< ArrayList<Integer> > similituds = new ArrayList();
     
     public LlistaProductes(){};
+    /**
+     * 
+     * @param arrayproducte 
+     * La llista de llistes similituds es actualitzada amb els graus de similitud afegits del nou producte en la arrayproducte
+     */
+    private void afegir_producte_similituds(ArrayList<Integer> arrayproducte){
+        for(int i = 0; i < similituds.size(); ++i){
+            if(i != similituds.size()-1){
+                ArrayList<Integer> ant = similituds.get(i);
+                ant.add(arrayproducte.get(i));
+                similituds.set(i, ant);
+            }
+        }
+    }
     
-    public void AfegirProducte(int id){
+    private void borrar_producte_similituds(int id){
+        for(int i = 0; i < similituds.size(); ++i){
+            ArrayList<Integer> ant = similituds.get(i);
+            ant.remove(id);
+            similituds.set(i,ant);
+        }
+    }
+    
+    /**
+     * 
+     * @param id
+     * @return La array de similituds del producte amb identificador id amb la resta de productes de la llista
+     */
+    public ArrayList<Integer> consultar_array_producte(int id){
+        return similituds.get(id);
+    }
+    
+    public void AfegirProducte(int id, ArrayList<Integer> arrayProducte){
         this.getArrayproductes().add(id);
         this.setContaproductes(getArrayproductes().size());
+        similituds.add(id,arrayProducte);
+        afegir_producte_similituds(arrayProducte);
     }
     
     public void BorrarProductePosicio(int posicio){
         this.getArrayproductes().remove(posicio);
         this.setContaproductes(getArrayproductes().size());
+        similituds.remove(posicio);
+        borrar_producte_similituds(posicio);
     }
     
     public void BorrarProducte(int idproducte){
         this.getArrayproductes().remove(idproducte);
         this.setContaproductes(getArrayproductes().size());
+        similituds.remove(idproducte);
+        borrar_producte_similituds(idproducte);
     }
 
     /**
